08d11cc8368741399a9456f853048f086b2337e1
unknown <girish6j87@gmail.com>
2022-12-21 20:59:36 +0530

    Initial commit

---
+++ src/main/java/com/gcp/revisitspring/RevisitSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gcp.revisitspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisitSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisitSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcp/revisitspring/RevisitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcp.revisitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisitSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
